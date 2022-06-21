7c8645480b37a701c67ffd493871efcfbd61ac25
Call-X <emile.miath.architecte@hotmail.com>
2022-06-21 14:31:49 +0200

    FC

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles.apps.ProfilesConfig',
+    'lettings.apps.LettingsConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,4 +108,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+# LOGIN_URL = '/home/'
+# LOGIN_REDIRECT_URL = '/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
