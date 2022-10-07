97692e2624b1eda6d3935d6e9b258f6a82fe1fa4
Call-X <emile.miath.architecte@hotmail.com>
2022-10-07 17:32:13 +0200

    update config.yml settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET KEY not found in .env').split(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'ALLOWED HOSTS not found in .env').split(",")  
 
