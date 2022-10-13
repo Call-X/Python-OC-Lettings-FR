9f7fdb18fe9fd3ce5c4a3d223ffb845c6bdeb646
Call-X <emile.miath.architecte@hotmail.com>
2022-10-13 15:34:53 +0200

    new update

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ RUN pip install --upgrade setuptools
 RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN pip3 install -r requirements.txt
 
-EXPOSE 8000
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+# EXPOSE 8000
+# CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD gunicorn oc_lettings_site.wsgi:application --bind 0.0.0.0:$PORT
 
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,9 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET KEY not found in .env').split(
 
 DEBUG = os.environ.get('DEBUG')
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'ALLOWED HOSTS not found in .env').split(",")  
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'ALLOWED HOSTS not found in .env').split(",")  
 
 # Application definition
 
