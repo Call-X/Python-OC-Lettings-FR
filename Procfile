d80c262cbe7236ba5aa0ec73330a0240923a89cd
Call-X <emile.miath.architecte@hotmail.com>
2023-01-05 11:02:44 +0100

    Procfile and settings.pyupdates

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: gunicorn oc_lettings_site.wsgi:application --log-file --log-level debug
-heroku ps:scale web=1
+heroku ps:scale web=1:eco -a oclettingswebapp
 release: python manage.py migrate 
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,8 +120,7 @@ STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-# LOGIN_URL = '/home/'
-# LOGIN_REDIRECT_URL = '/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
