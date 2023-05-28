98f73631eca630585e1d905bc364b71cef93f3db
Call-X <emile.miath.architecte@hotmail.com>
2023-05-28 15:02:52 +0200

    config.yml et settings.py update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,15 +5,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from dotenv import load_dotenv
 load_dotenv()
 
-sentry_sdk.init(
-    os.environ.get('DNS_SENTRY'),
-    integrations=[
-        DjangoIntegration(),
-    ],
-    traces_sample_rate=1.0,
-    send_default_pii=True
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,6 +124,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+sentry_sdk.init(
+    os.environ.get('DNS_SENTRY'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
+SENTRY_SECURITY_TOKEN= os.environ.get('SENTRY_SECURITY_TOKEN')
 
 
 
